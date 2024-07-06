b45ff1d0cd5ef3f03b16fbfaf86992d3fd2c0db3
nteguem roland <nteguemroland@gmail.com>
2024-07-06 07:33:16 +0100

    rechnge port

--- app-test.js
+++ app-test.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 2500;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
